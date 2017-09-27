8c83b80692343b2ab56676383b70ed11cd306a4f
jiangge <jiangge@qiyi.com>
2017-09-27 19:02:51 +0800

    set rack as None if no rack info

--- resource-managers/yarn/src/main/scala/org/apache/spark/scheduler/cluster/YarnScheduler.scala
+++ resource-managers/yarn/src/main/scala/org/apache/spark/scheduler/cluster/YarnScheduler.scala
@@ -17,13 +17,6 @@
 
 package org.apache.spark.scheduler.cluster
 
-import org.apache.hadoop.yarn.util.RackResolver
-import org.apache.log4j.{Level, Logger}
-
-import org.apache.spark._
-import org.apache.spark.scheduler.TaskSchedulerImpl
-import org.apache.spark.util.Utils
-
 private[spark] class YarnScheduler(sc: SparkContext) extends TaskSchedulerImpl(sc) {
 
   // RackResolver logs an INFO message whenever it resolves a rack, which is way too often.
@@ -34,6 +27,11 @@ private[spark] class YarnScheduler(sc: SparkContext) extends TaskSchedulerImpl(s
   // By default, rack is unknown
   override def getRackForHost(hostPort: String): Option[String] = {
     val host = Utils.parseHostPort(hostPort)._1
-    Option(RackResolver.resolve(sc.hadoopConfiguration, host).getNetworkLocation)
+    val rackNode = RackResolver.resolve(sc.hadoopConfiguration, host)
+    if (rackNode.getNetworkLocation.equals(NodeBase.normalize(NetworkTopology.DEFAULT_RACK))) {
+      None
+    } else {
+      Option(rackNode.getNetworkLocation)
+    }
   }
 }
